d1c828546dc85d343c1dad7f778e55170e7d4351
Mykhailo Holub <mishataube2005@gmail.com>
2024-09-11 09:15:53 +0300

    Update main.py

--- task_1/main.py
+++ task_1/main.py
@@ -1,12 +1,23 @@
 from Set import Set
 from random import random
 
-def random_set(length, multiply_by, offset):
-    set = Set()
-    for i in range(length):
-        set.insert(random()*multiply_by + offset)
-    return set
+class UncleFester_SkilledTester:
+    def __init__(self):
+        pass
+
+    def random_set(length, minimum, maximum, integer = False):
+        set = Set()
+        for i in range(length):
+            set.insert(random() * multiply_by + offset)
+        return set
+
+    def random_int_set(self, range, minimum):
+        set = Set()
+        for i in range(length):
+            set.insert(random() * multiply_by + offset)
+        return set
 
 
-while True:
-    print(str(random_set(int(input("length>>>")), 1000, -500)))
+
+
+
