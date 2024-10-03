2437131d6d29c02311770c2372bb2b515718096e
Mykhailo Holub <mishataube2005@gmail.com>
2024-10-03 17:32:03 +0300

    start

--- task_4/RandomGraph.py
+++ task_4/RandomGraph.py
@@ -1,12 +1,13 @@
 import Graph
-import random
+from random import random
+import copy
 
 class RandomGraph(Graph.Graph):
     def __init__(self, n_vertices, probability):
         super().__init__([[False]*n_vertices]*n_vertices)
         for row_n in range(self.size):
             for element_n in range(row_n,self.size):
-                if random.random()<probability:
+                if random()<probability:
                     self.matrix[row_n][element_n] = True
                     self.matrix[element_n][row_n] = True
 
@@ -15,16 +16,20 @@ class RandomOrientedGraph(Graph.Oriented_Graph):
         super().__init__([[False]*n_vertices]*n_vertices)
         for row_n in range(self.size):
             for element_n in range(self.size):
-                if random.random()<probability:
+                if random()<probability:
                     self.matrix[row_n][element_n] = True
 
 class RandomWeightedGraph(Graph.WeightedGraph):
     def __init__(self, n_vertices, probability, min_val, max_val, is_int = False):
-        super().__init__([[False] * n_vertices] * n_vertices)
+        start_row = [False] * n_vertices
+        y = []
+        for i in range(n_vertices):
+            y.append(copy.deepcopy(start_row))
+        super().__init__(y)
         for row_n in range(self.size):
             for element_n in range(row_n,self.size):
-                if random.random()<probability:
-                    weight = random.random()*(max_val-min_val)+min_val
+                if random()<probability:
+                    weight = random()*(max_val-min_val)+min_val
                     if is_int:
                         weight = int(weight)
                     self.matrix[row_n][element_n] = weight
@@ -35,8 +40,8 @@ class RandomOrienedWeightedGraph(Graph.OrientedWightedGraph):
         super().__init__([[False] * n_vertices] * n_vertices)
         for row_n in range(self.size):
             for element_n in range(self.size):
-                if random.random()<probability:
-                    weight = random.random()*(max_val-min_val)+min_val
+                if random()<probability:
+                    weight = random()*(max_val-min_val)+min_val
                     if is_int:
                         weight = int(weight)
                     self.matrix[row_n][element_n] = weight
