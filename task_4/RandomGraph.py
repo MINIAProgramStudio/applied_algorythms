6748eace2d1be5c64f8a5ab23f73a983b78c3eae
Mykhailo Holub <mishataube2005@gmail.com>
2024-10-03 17:58:47 +0300

    bug removed

--- task_4/RandomGraph.py
+++ task_4/RandomGraph.py
@@ -4,7 +4,11 @@ import copy
 
 class RandomGraph(Graph.Graph):
     def __init__(self, n_vertices, probability):
-        super().__init__([[False]*n_vertices]*n_vertices)
+        start_row = [False] * n_vertices
+        y = []
+        for i in range(n_vertices):
+            y.append(copy.deepcopy(start_row))
+        super().__init__(y)
         for row_n in range(self.size):
             for element_n in range(row_n,self.size):
                 if random()<probability:
@@ -13,7 +17,11 @@ class RandomGraph(Graph.Graph):
 
 class RandomOrientedGraph(Graph.Oriented_Graph):
     def __init__(self, n_vertices, probability):
-        super().__init__([[False]*n_vertices]*n_vertices)
+        start_row = [False] * n_vertices
+        y = []
+        for i in range(n_vertices):
+            y.append(copy.deepcopy(start_row))
+        super().__init__(y)
         for row_n in range(self.size):
             for element_n in range(self.size):
                 if random()<probability:
@@ -21,7 +29,7 @@ class RandomOrientedGraph(Graph.Oriented_Graph):
 
 class RandomWeightedGraph(Graph.WeightedGraph):
     def __init__(self, n_vertices, probability, min_val, max_val, is_int = False):
-        start_row = [False] * n_vertices
+        start_row = [None] * n_vertices
         y = []
         for i in range(n_vertices):
             y.append(copy.deepcopy(start_row))
@@ -37,7 +45,11 @@ class RandomWeightedGraph(Graph.WeightedGraph):
 
 class RandomOrienedWeightedGraph(Graph.OrientedWightedGraph):
     def __init__(self, n_vertices, probability, min_val, max_val, is_int = False):
-        super().__init__([[False] * n_vertices] * n_vertices)
+        start_row = [None] * n_vertices
+        y = []
+        for i in range(n_vertices):
+            y.append(copy.deepcopy(start_row))
+        super().__init__(y)
         for row_n in range(self.size):
             for element_n in range(self.size):
                 if random()<probability:
