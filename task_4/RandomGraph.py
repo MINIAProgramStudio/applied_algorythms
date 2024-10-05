b534686c30e8eea740343eed0b63bdb746c43d88
Mykhailo Holub <mishataube2005@gmail.com>
2024-10-05 23:40:00 +0300

    add PreciseRandomWeightedGraph

--- task_4/RandomGraph.py
+++ task_4/RandomGraph.py
@@ -57,3 +57,22 @@ class RandomOrienedWeightedGraph(Graph.OrientedWightedGraph):
                     if is_int:
                         weight = int(weight)
                     self.matrix[row_n][element_n] = weight
+
+class PreciseRandomWeightedGraph(Graph.WeightedGraph):
+    def __init__(self, n_vertices, probability, min_val, max_val, is_int = False):
+        start_row = [None] * n_vertices
+        y = []
+        for i in range(n_vertices):
+            y.append(copy.deepcopy(start_row))
+        super().__init__(y)
+        counter = 0
+        while counter < probability:
+            row_n = int(random()*n_vertices)
+            element_n = int(random()*n_vertices)
+            if self.matrix[row_n][element_n] is None:
+                weight = random()*(max_val-min_val)+min_val
+                if is_int:
+                    weight = int(weight)
+                self.matrix[row_n][element_n] = weight
+                self.matrix[element_n][row_n] = weight
+                counter+=1
